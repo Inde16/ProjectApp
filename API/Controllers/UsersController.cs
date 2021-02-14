adb657fe15d9723d80bf9da76d4e2e8fcffcc0e1
sotiris pissaris <sotinde@gmail.com>
2021-02-14 12:13:41 +0200

    Skeleton_1

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // api/users/3
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            return user;
+        }
+
+
+    }
+}
